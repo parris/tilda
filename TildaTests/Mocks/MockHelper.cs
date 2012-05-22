dc7a0c559f520132c94c05066c0b251669470082
Parris Khachi <parrisk@netapp.com>
2012-05-22 16:10:48 -0700

    Deep copy for mocks

---
+++ TildaTests/Mocks/MockHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TildaTests.Mocks {
+    class MockHelper {
+
+        /**
+         * Makes a deep copy for some object where its class can be [Serializable]
+         * @param T a generic object
+         * @return T a deep copy of that object
+         */
+        public static T DeepClone<T>(T obj) {
+            using(var ms = new MemoryStream()) {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
